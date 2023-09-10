440f91c6d19244d74736d40e8581b59e2f6d579f
Joletuar <joletuar@gmail.com>
2023-09-10 01:18:33 -0500

    Proyecto terminado

--- src/guests/components/NewGuestForm.tsx
+++ src/guests/components/NewGuestForm.tsx
@@ -292,7 +292,7 @@ export const NewGuestForm = () => {
                 className='w-full  text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
                 disabled={isFetching}
               >
-                Registrrar
+                Registrar
               </button>
             </form>
           </div>
